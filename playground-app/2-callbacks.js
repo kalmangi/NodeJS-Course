7e4372bef4b001de123d40c8404b7b2bf8620ab2
unknown <aishwarya.kalmangi@tarento.com>
2020-03-04 12:03:49 +0530

    Promises

--- playground-app/2-callbacks.js
+++ playground-app/2-callbacks.js
@@ -4,6 +4,21 @@ const add = (a, b, callback) =>{
     }, 2000)
 }
 
+
+
 add(2,3,(sum)=>{
     console.log(sum)
+})
+
+const doWorkCallback = (callback) =>{
+    setTimeout(()=>{
+        callback(undefined,'this')
+    }, 2000)
+}
+
+doWorkCallback((error, result)=>{
+    if(error){
+        return console.log(result)
+    }
+    console.log(result)
 })
--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,28 +1,22 @@
-console.log("This is app.js")//This is client side JS which is gng to run in the browser.
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
 
-
-const weatherForm = document.querySelector('form');
-const search = document.querySelector('input');
-const msg = document.querySelector('#msg-1');
-msg.textContent="From JS";
-
-weatherForm.addEventListener('submit',(e)=>{
-    e.preventDefault();//This method will prevent browser from refreshing if we submit button.
-    //Its going to prevent default behaviour which is to refresh the browser allowing the server to render a new page.
-    const location = search.value;
-    fetch("/weather?address=!"+location).then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error);
-        }else{
-            console.log(data);
-            console.log(data.location);
-        }
-        
-    })//This is going to run when json data has arrived.
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
 })
-    console.log(location);
-
-})//function is gng to call every single time form is submitted.
-
