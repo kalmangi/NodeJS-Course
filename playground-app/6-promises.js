57f84c50496ce0b2d5e776ec7e85d19dc96961c7
unknown <aishwarya.kalmangi@tarento.com>
2020-03-06 12:55:59 +0530

    async await

--- playground-app/6-promises.js
+++ playground-app/6-promises.js
@@ -1,3 +1,5 @@
+//import { set } from "mongoose"
+
 const doNewPromise = new Promise((resolve,reject)=>{
     setTimeout(()=>{
         resolve([1,2,3])
@@ -9,5 +11,46 @@ doNewPromise.then((result)=>{
 }).catch((error)=>{
     console.log('Error',error)
 })
+const add = (a,b)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(a+b)
+        },3000)
+    })
+}
+
+const add1 = (a,b)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(a+b)
+        },3000)
+    })
+}
+
+//This will keeping summing up the result: 8,13. This is not promise chaining. This is just nesting async operations
+add(5,3).then((sum)=>{
+    console.log(sum);
+
+    add(sum, 1).then((sum2)=>{
+        console.log(sum2)
+    }).catch((error)=>{
+        console.log('Error',error)
+    })
+}).catch(error=>{
+    console.log(error)
+})
+
+//Promise Chaining
+
+add1(1,1).then((sum)=>{
+    return add(sum, 5);
+}).then((sum)=>{
+    console.log(sum)
+}).catch((error)=>{
+    console.log(error)
+})
+
+//If we want to access to both of these two sums together its not easy.We should do something like save it to db and send them to user.
+//Or we have to create variables in parent scope and assign in the add function.
 
 
--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+//const validator = require('validator')
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",{
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
 })
 
 // const User = mongoose.model('User',{
@@ -10,12 +12,42 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",{
 //     },
 //     age: {
 //         type: Number,
+//         default: 0,
+//          validate(value) {
+//                 if(value<0){
+//                     throw new Error('Must be positive')
+//                 }
+//             }
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+
+//         validate(value){
+//             if(!validator.isEmail(value)){
+//                 console.log('Email is invalid')
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         minlength: 7,
+//         trim: true,
+//         validate(value){
+//             if(value.toLowerCase().includes('password')){
+//                 throw new Error('Incorrect password')
+//             }
+//         }
 //     }
 // })
 
 // const a=new User({
 //     name: "a",
-//     age: 24
+//     age: 24,
+//     email: "a@g",
+//     password: 'passwosrdd'
 // })
 
 // a.save().then(()=>{
@@ -24,22 +56,25 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",{
 //     console.log(error)
 // })
 
-const Tasks = mongoose.model('Task',{
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-});
+// const Tasks = mongoose.model('Task',{
+//     description: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     }
+// });
 
-const Task = new Tasks({
-    description: "a",
-    completed: true
-});
+// const Task = new Tasks({
+//     description: "Eat Lunch",
+//     //completed: true
+// });
 
-Task.save().then(()=>{
-    console.log(a)
-}).catch((error)=>{
-    console.log(error)
-})
+// Task.save().then(()=>{
+//     console.log(Task)
+// }).catch((error)=>{
+//     console.log(error)
+// })
